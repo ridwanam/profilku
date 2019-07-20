c1f5cad562f9c2f5154542cf4fb49479d6a062de
ridwan <ridwan.22693@gmail.com>
2019-07-20 14:09:46 +0700

    install repo

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Profil Ku</title>
+    <link rel="stylesheet"
+  href="https://fonts.googleapis.com/css?family=Montserrat">
+    <link rel="stylesheet" href="profil.css">
+  </head>
+  <body>
+      <div class="" id="main">
+        <img width="200px" height="300px"src="ridwan.jpg" alt="no pict">
+          <div class="text">
+            <h1>Ridwan Ahmad Ma'arif</h1>
+            <p>Jakarta, 06 November 1998</p>
+            <p>Mahasiswa Informatika, Universitas Nasional</p>
+          </div>
+      </div>
+  </body>
+</html>
